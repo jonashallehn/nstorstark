7d993c28d1cc576bd1d08b8369693a7311ba867d
jonashallehn <jonas.hallehn@gmail.com>
2017-11-21 14:53:53 +0100

    allt är inlagt på startsidan

--- wp-content/themes/nstor/page-template/home.php
+++ wp-content/themes/nstor/page-template/home.php
@@ -10,13 +10,78 @@ Template Name: Home
 <div class="row header">
   <img src="<?php the_field('hero_image') ?>">
 </div>
+
 <div class="row background">
+
   <div class="col-md-6">
-    <img src="<?php the_field('beergif') ?>">
+    <div class="bottle1">
+    <div class="bottle">
+      <img src="<?php the_field('beergif') ?>">
+    </div>
+    </div>
   </div>
+
   <div class="col-md-6">
-    <img src="<?php the_field('bootlesgif') ?>">
+    <div class="gif1">
+      <img src="<?php the_field('bootlesgif') ?>">
+    </div>
   </div>
+
+  <div class="col-md-6">
+    <div class="welcome">
+      <a href="<?php the_field('welcome_link') ?>"><h1>Välkommen</br></h1></a>
+      <p><?php the_field('textfield_welcome') ?></p>
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="news">
+      <a href="<?php the_field('news_link') ?>"><h1>Nyheter</h1></a>
+      <p><?php the_field('textfield_news') ?></p>
+    </div>
+  </div>
+
+  <div class="col-sm-3">
+      <div class="instagram">
+        <a href="<?php the_field('instagram_link') ?>"><h1>Instagram</h1></a>
+      </div>
+  </div>
+
+  <div class="col-sm-3">
+    <div class="concept">
+      <p><?php the_field('textfield_concept') ?></p>
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="facebook">
+      <a href="<?php the_field('facebook_link') ?>"><h1>Facebook</h1></a>
+      <img src="<?php the_field('facbook') ?>">
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="assortment">
+      <a href="<?php the_field('sortiment_link') ?>"><h1>Sortiment</h1></a>
+      <img src="<?php the_field('sortiment_picture') ?>">
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="green">
+    <div class="story">
+      <a href="<?php the_field('story_link') ?>"><h1>Vår berättelse</h1></a>
+      <p><?php the_field('textfield_story') ?></p>
+    </div>
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="investor">
+      <p><?php the_field('textfield_investor') ?></p>
+    </div>
+  </div>
+
 </div>
 
 <?php get_footer(); ?>
