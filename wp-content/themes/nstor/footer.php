8e497411ca8fd86739b09e2ba959a9756efd1725
jonashallehn <jonas.hallehn@gmail.com>
2017-12-02 17:23:42 +0100

    lots of stuff

--- wp-content/themes/nstor/footer.php
+++ wp-content/themes/nstor/footer.php
@@ -5,5 +5,29 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
+<div class="footer">
+  <div class="left_footer">
+  <ul>
+    <li>N Stor</li>
+    <li>Storgatan 53</li>
+    <li>703 63 Örebro</li></br>
+    <li>Org.nummer: 556850-1265</li>
+  </ul>
+</div>
+
+<div class="right_footer">
+  <ul>
+    <li>Frågor?</li>
+    <li>019-277 11 00</li>
+    <li>info@nstorstark.se</li></br>
+    <a href="https://www.facebook.com/nstorstarkofficial"<img src="../Webb/images/facebook.png" alt="facebook" height="42" width="42">facebook</a>
+    <a href="https://www.instagram.com/nstorstarkofficial/"<img src="../Webb/images/instagram.svg" alt="instagram" height="42" width="42">instagram</a>
+  </ul>
+
+</div>
+</div>
+
+  </section>
+
 </body>
 </html>
--- wp-content/themes/nstor/page-template/konceptdrycker.php
+++ wp-content/themes/nstor/page-template/konceptdrycker.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Home
+Template Name: Konceptdrycker
 */
 ?>
 
@@ -14,10 +14,12 @@ Template Name: Home
 <div class="row background">
 
   <div class="col-md-6">
+    <div class="bottle1">
     <div class="bottle">
       <img src="<?php the_field('beergif') ?>">
     </div>
     </div>
+  </div>
 
   <div class="col-md-6">
     <div class="gif1">
@@ -26,62 +28,89 @@ Template Name: Home
   </div>
 
   <div class="col-md-6">
+    <div class="green">
     <div class="welcome">
-      <h1><?php the_field('welcome_text') ?></h1>
-      <p><?php the_field('welcome_textfield') ?></p>
+      <h1><a href="<?php the_field('header_konceptdryck') ?>"></h1></a>
+      <p><?php the_field('textfield_konceptdrycker') ?></p>
     </div>
-    </div>
-
-  <div class="col-md-6">
-    <div class="news">
-      <h1"<?php the_field('news_link') ?>"></h1>
-      <p><?php the_field('textfield_news') ?></p>
     </div>
   </div>
 
   <div class="col-md-6">
-      <div class="instagram">
-        <a href="<?php the_field('instagram_link') ?>"></a>
-      </div>
-      </div>
+    <a href="<?php the_field('news_link') ?>">
+    <div class="green">
+    <div class="news">
+      <a href="<?php the_field('news_link') ?>"><h1>Nyheter</h1></a>
+      <p><?php the_field('textfield_news') ?></p>
+    </div>
+    </div>
+    </a>
+  </div>
 
-  <div class="col-md-6">
+  <div class="col-sm-3">
+    <div class="green1">
+      <div class="instagram">
+        <a href="<?php the_field('instagram_link') ?>"><h1>Instagram</h1></a>
+        <section class="instagram-feed">
+            <div id="instafeed"</div>
+        </section>
+      </div>
+      </div>
+  </div>
+
+  <div class="col-sm-3">
+    <div class="green1">
     <div class="concept">
       <p><?php the_field('textfield_concept') ?></p>
     </div>
     </div>
+  </div>
 
   <div class="col-md-6">
     <a href="https://www.facebook.com/nstorstarkofficial/">
+    <div class="green">
     <div class="facebook">
-      <div class="facelogo">
-      <img src="<?php the_field('facbook') ?>"></img>
-    </div>
+      <h1>Facebook</h1>
+        <div class="face">
+      <img src="<?php the_field('facbook') ?>">
     </div>
   </div>
+  </div>
     </a>
+  </div>
 
 
   <div class="col-md-6">
+    <div class="green">
     <div class="assortment">
-      <a href="<?php the_field('sortiment_link') ?>"></a>
+      <a href="<?php the_field('sortiment_link') ?>"><h1>Sortiment</h1></a>
       <img src="<?php the_field('sortiment_picture') ?>">
     </div>
     </div>
+  </div>
 
   <div class="col-md-6">
+    <div class="green">
     <div class="story">
-      <a href="<?php the_field('story_link') ?>"></a>
+      <a href="<?php the_field('story_link') ?>"><h1>Vår berättelse</h1></a>
       <p><?php the_field('textfield_story') ?></p>
     </div>
     </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="green">
+    <div class="investor">
+      <p><?php the_field('textfield_investor') ?></p>
+    </div>
+    </div>
+  </div>
 
   <div class="col-md-6">
     <div class="investor">
       <p><?php the_field('textfield_investor') ?></p>
     </div>
-    </div>
-
+  </div>
 
 </div>
 
